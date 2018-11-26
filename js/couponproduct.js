ad9c244f715a1a55a7d7bc6a96a3773d88b7d6dc
IDorry <1767771542@qq.com>
2018-11-26 15:05:39 +0800

    修改小bug

--- js/couponproduct.js
+++ js/couponproduct.js
@@ -3,6 +3,11 @@ $(function(){
     // 获取优惠券标题id
     var couponid = getUrlId("couponid");
     var src;
+    var srcAll = [];
+
+    // 左右箭头
+    var $arr_left = $('.mmb_mask .arr_left');
+    var $arr_right = $('.mmb_mask .arr_right');
 
     // 1. 渲染商品列表
     $.ajax({
@@ -17,6 +22,14 @@ $(function(){
             // 绑定数据
             var couproListStr = template("couproList", info);
             $('.mmb_couponProduct .couproBox').html( couproListStr );
+
+            // 保存所有图片
+            // console.log(info.result);
+            for(i=0;i<=info.result.length;i++){
+                // srcAll.push( info.result[i]["couponProductImg"] );
+                
+            }
+            console.log(srcAll);
         }
     });
 
@@ -24,7 +37,7 @@ $(function(){
     $(".couproBox").on("click", ".couproList", function(){
         // 获取保存被点击项的图片
         src = $(this).find("a .img img").attr("src");
-        // console.log(src);
+        console.log(src);
         // 将获取到的图片  给  模态框上的图片
         $('.mmb_mask .mask_img').find("img").attr("src", src);
         // 让模态框显示
@@ -32,9 +45,16 @@ $(function(){
     });
 
     // 点击模态框的delete按钮时，关闭模态框
-    $('.mmb_mask').on("click", ".delete", function(){
+    $(".delete").click(function(){
         $('.mmb_mask').hide();
         // 把模态框的图片清空
         $(this).prev().attr("src", "");
     });
+
+
+    // 点击模态框的左箭头，显示上一张图片
+    $arr_left.click(function(){
+        console.log(1111);
+    });
+
 });
--- js/moneyctrl.js
+++ js/moneyctrl.js
@@ -32,7 +32,7 @@ $(function(){
                 $('.paging .currPage span').text( (pageid+1)+"/"+totalPage ); 
     
                 // 3. 分页部分
-                var htmlStr = template("pageCount", {pageCount: pageCount});
+                var htmlStr = template("pageCount", {pageCount: totalPage});
                 $('.currPage .morePage').html( htmlStr );
     
             }
@@ -41,41 +41,59 @@ $(function(){
 
 
 
-    //点击下一页按钮，将当前页数+1，重新渲染页面
+    //2.点击下一页按钮，将当前页数+1，重新渲染页面
     $next.on("click", function(){
+
+        // 判断下一页是否被禁用
+        if( $('.prev').hasClass("mui-disabled") ){
+            $('.prev').removeClass("mui-disabled");
+        }
+
         pageid++;
+
         $('.currPage span').text( pageid+"/"+totalPage );
         
         if( pageid >= totalPage ){
             pageid = totalPage-1;
+            // 最后一页时，禁用下一页按钮     
+            $(this).addClass("mui-disabled");
+            return; 
         }
         
         render( pageid );
     });
-    //点击上一页按钮，将当前页数-1，重新渲染页面
+    //3. 点击上一页按钮，将当前页数-1，重新渲染页面
     $prev.on("click", function(){
+
+        // 判断下一页是否被禁用
+        if( $('.next').hasClass("mui-disabled") ){
+            $('.next').removeClass("mui-disabled");
+        }
+
         pageid--;
         // console.log(pageid);
         // console.log(totalPage);
+
         if( pageid <= 0 ){
-            pageid = 0;
-            $('.currPage span').text( pageid+"/"+totalPage );            
-            // $(this).toggleClass("mui-disabled").next(".prev").toggleClass("mui-disabled");            
+            pageid = 0;   
+            // 第一页时，禁用上一页按钮  
+            $(this).addClass("mui-disabled"); 
+            // return;          
         }
         render( pageid );
     }); 
     
-    // 点击当前页(1/3的那个框)，切换分页标签(下面的ul的显示与隐藏)
+    //4. 点击当前页(1/3的那个框)，切换分页标签(下面的ul的显示与隐藏)
     $curr.on("click", function(){
-        $(this).find('.morePage').toggle();
+        $(this).find(".morePage").toggle();
         // toggle()----切换状态
     });
 
-    // 点击显示出来的页数列表的项，跳转到对应页数，重新渲染这个页面
+    //5. 点击显示出来的页数列表的项，跳转到对应页数，重新渲染这个页面
     $curr.on("click", ".morePage li", function(){
         // console.log(111);
         // 点击后，记录点击的哪一页
-        pageid = $(this).index() + 1;
+        pageid = $(this).index();
         $(this).parent().siblings('span').text( $(this).text() );
         // console.log(pageid);
         render(pageid);
